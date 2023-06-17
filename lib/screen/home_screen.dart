cfc7a11bc77b760444539f8d801f18e3e2a029e9
rachmadyassera <rachmad.yasser.a@gmail.com>
2023-06-17 13:20:13 +0700

    tahap 1, membuat ui appbar ewaller, video materi sampai menit 4:40

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [
+            Colors.blue.shade800,
+            Colors.white60
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight
+          )
+        ),
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.only(top: 40),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Padding(padding: EdgeInsets.symmetric(horizontal: 8.0),
+                  child: CircleAvatar(
+                    radius: 22,
+                    backgroundColor: Colors.blueGrey,
+                    child: CircleAvatar(
+                      radius: 20,
+                      backgroundImage: NetworkImage('https://i.pravatar.cc/150?img=11'),
+                    ),
+                  ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          "Hallo, ",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          "Rachmad Yasser Al Zuhri",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w700,
+                            fontSize: 15,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  IconButton(
+                    icon: Stack(
+                      children: const [
+                        Icon(
+                          Icons.notifications_none_rounded,
+                          color: Colors.white,
+                          size: 30,
+                        ),
+                        Positioned(
+                          top: 0.0,
+                          right: 0.0,
+                          child: Icon(
+                            Icons.brightness_1,
+                            size: 8.0,
+                            color: Colors.redAccent,
+                          ),
+                          )
+                      ],
+                    ),
+                    onPressed: (){},
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+
+    );
+  }
+}
